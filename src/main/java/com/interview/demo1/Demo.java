92b5800d6bf0f6d90c65498abda129c1a554dc39
wangxingang <wangxingang0610@sina.com>
2021-02-21 22:03:41 +0800

    init

---
+++ src/main/java/com/interview/demo1/Demo.java
@@ -0,0 +1,9 @@
+package com.interview.demo1;
+
+public class Demo {
+    public static void main(String[] args) {
+        Integer a = 128;
+        Integer b = 128;
+        System.out.println(a == b);
+    }
+}
